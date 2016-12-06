8fb39b43b02c33a296b388760d6b5dae0fcc67ac
Mangirdas <m.judeikis@gmail.com>
2016-12-06 18:50:59 +0000

    init

---
+++ 8/test/maven-test-app/src/main/java/xxxxxx/yyyyyy/zzzzzz/HelloController.java
@@ -0,0 +1,16 @@
+package xxxxxx.yyyyyy.zzzzzz;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    String hello() {
+        return "Hello World!";
+    }
+
+    
+}
